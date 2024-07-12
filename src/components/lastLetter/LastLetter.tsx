57073b1cf18092842efb04e1648201a3263e61b4
Paula Cano <paulacanoroman@gmail.com>
2024-07-12 12:21:31 +0200

    feat: lastletter function

--- src/components/lastLetter/LastLetter.tsx
+++ src/components/lastLetter/LastLetter.tsx
@@ -4,10 +4,24 @@ import { useState } from "react"
 const LastLetter = () => {
 
     const [sentence, setSentence] = useState<string>()
+
+    const changeSentence = ()=>{
+        const arrWords = sentence?.split(' ');
+        const words = arrWords?.map((oneWord)=>{
+            const letters = oneWord.split('').reverse();
+            const lastLetter = letters[0].toUpperCase()
+            letters[0] = lastLetter
+            return letters.reverse().join('')
+        })
+        setSentence(words?.join(` `))
+    }
   return (
     <>
     <h3>Change your sentence, every word in capital letters</h3>
     <input onChange={(e)=>{setSentence(e.target.value)}}></input>
+    <button onClick={()=>{changeSentence()}}>change</button>
+    <br></br>
+    {sentence}
     </>
   )
 }
