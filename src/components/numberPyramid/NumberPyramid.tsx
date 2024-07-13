f762820ba1818e0c680d5f64a9a8fc6beb40c625
Paula Cano <paulacanoroman@gmail.com>
2024-07-13 22:57:07 +0200

    feat console.log triangle

--- src/components/numberPyramid/NumberPyramid.tsx
+++ src/components/numberPyramid/NumberPyramid.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
 
 const NumberPyramid = () => {
+
+    const [userNumber, setUserNumber] = useState<number>(3)
+
+    const convertPyramid = ()=>{
+        for(let i = 0; i <= userNumber; i++){
+            for(let x=0; x<i; x++){
+                console.log(i)
+            }
+            document.write('<br>')
+        }
+    }
   return (
-    <div>NumberPyramid</div>
+    <>
+    <input type="number" onChange={(e)=>{setUserNumber(parseInt(e.target.value))}}></input>
+    <button onClick={()=>{convertPyramid()}}>pyramid</button>
+    <div id="testnovia"></div>
+    </>
   )
 }
 
